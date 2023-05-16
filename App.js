b20db1364bfb5a563a7c081a0e949d52c3775b1a
Yunus-Mulla <133741681+Yunus-Mulla@users.noreply.github.com>
2023-05-16 13:39:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Googlemeet from './Google meet/Googlemeet'
+
+
+const App = () => {
+  return (
+    <div ClassName='App'>
+     <Googlemeet/>
+    </div>
+  )
+}
+
+export default App
